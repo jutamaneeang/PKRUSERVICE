e91f3c100e711d53c63b9262ad62eacc56acb434
422Mac01 <422mac01@422Mac01s-iMac.local>
2017-07-28 16:00:37 +0700

    uplodaok

--- SignUpViewController.swift
+++ SignUpViewController.swift
@@ -62,6 +62,25 @@ class SignUpViewController: UIViewController {
         let strPHP: String = strFontPHP + "\(strName)&User=\(strUser)&Password=\(strPassword)"
         print("strPHP ==> \(strPHP)")
         
+        let myURL = URL(string: strPHP)
+        let request = NSMutableURLRequest(url: myURL!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            data, response, error in
+            
+            if error != nil{
+                print("Error ==> \(String(describing: error))")
+            }else{
+                if let unwrappedData = data{
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    let strResult = dataString as Any
+                    print("strResult ==> \(strResult)")
+                }
+            }
+        }
+        task.resume()
+        
+        
+        
         
     } //uploadToServer
     
