37b0aef069998532cb01b886ece51a931e0ffab6
422Mac01 <422mac01@422Mac01s-iMac.local>
2017-07-28 14:23:31 +0700

    startcord

--- SignUpViewController.swift
+++ SignUpViewController.swift
@@ -9,14 +9,33 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var alertLavel: UILabel!
+    
+    @IBAction func saveButton(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
